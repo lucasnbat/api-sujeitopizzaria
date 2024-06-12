7281f78239e03a42e3aadcc0d1e87b805f336ae7
lucasnbat <lucasnunesb6@gmail.com>
2024-06-12 11:14:40 -0400

    funcionalidades: listar categoria

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 const router = Router();
 
@@ -16,5 +17,6 @@ router.get('/me', isAuthenticated, new DetailUserController().handle);
 /* Category Routes */
 
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router };
