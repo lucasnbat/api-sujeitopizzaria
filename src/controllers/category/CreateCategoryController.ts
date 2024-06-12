1071e0d1af1db9ae45b12249b9d39a2af974ed31
lucasnbat <lucasnunesb6@gmail.com>
2024-06-12 11:05:15 -0400

    funcionalidade criar categoria

--- src/controllers/category/CreateCategoryController.ts
+++ src/controllers/category/CreateCategoryController.ts
@@ -2,9 +2,11 @@ import { CreateCategoryService } from "../../services/category/CreateCategorySer
 import { Request, Response } from 'express'
 class CreateCategoryController {
     async handle(req: Request, res: Response) {
+        const { name } = req.body;
+
         const createCategoryService = new CreateCategoryService();
 
-        const category = await createCategoryService.execute();
+        const category = await createCategoryService.execute({ name });
 
         return res.json(category);
 
--- src/services/category/CreateCategoryService.ts
+++ src/services/category/CreateCategoryService.ts
@@ -1,8 +1,27 @@
 import prismaClient from "../../prisma";
 
+interface CategoryRequest {
+    name: string;
+}
+
 class CreateCategoryService {
-    async execute() {
-        return { ok: true }
+    async execute({ name }: CategoryRequest) {
+        
+        if (name === ''){
+            throw new Error('name invalid');
+        }
+
+        const category = await prismaClient.category.create({
+            data:{
+                name: name,
+            },
+            select:{
+                id: true,
+                name: true,
+            }
+        })
+
+        return category;
     }
 }
 
