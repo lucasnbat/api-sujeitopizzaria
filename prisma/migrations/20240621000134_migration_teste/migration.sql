28210689a43172c924b65ac11c5970b6c18c8d24
lucasnbat <lucasnunesb6@gmail.com>
2024-07-04 00:29:54 -0400

    (user)feat: editando rotas para possibilitar visualização de perfil por usuário

---
+++ prisma/migrations/20240621000134_migration_teste/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "teste" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "teste_pkey" PRIMARY KEY ("id")
+);
